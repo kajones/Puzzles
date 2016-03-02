9efcbebc76f5ef9ced9355406f904eeb7ca43a11
kajones <katherine.jones@btinternet.com>
2016-03-02 15:39:33 +0000

    LeetCode 5 & 6

---
+++ Puzzles.LeetCode/Problems_0001_0100/Problem_0005_LongestPalindromicSubstring.cs
@@ -0,0 +1,115 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Puzzles.LeetCode.Problems_0001_0100
+{
+    /// <summary>
+    /// Given a string S, find the longest palindromic substring in S. 
+    /// You may assume that the maximum length of S is 1000, 
+    /// and there exists one unique longest palindromic substring.
+    /// </summary>
+    [TestFixture]
+    public class Problem_0005_LongestPalindromicSubstring
+    {
+        [Test]
+        [TestCase("a", "a", "Single char")]
+        [TestCase("bb", "bb", "Two matching chars")]
+        [TestCase("ab", "a", "Two different chars so first char")]
+        [TestCase("ccd", "cc", "First two chars match")]
+        [TestCase("ABA", "ABA", "Whole odd string")]
+        [TestCase("ABBA", "ABBA", "Whole even string")]
+        [TestCase("asdfghgwoeih", "ghg", "Odd Substring")]
+        [TestCase("asjkllkjwi", "jkllkj", "Even substring")]
+        [TestCase("asasdfdsasb", "sasdfdsas", "Repeated char in palindrome")]
+        [TestCase("aaafghgxxxbnmnb", "bnmnb", "Longer of two")]
+        [TestCase("", "", "Empty string")]
+        public void RunTests(string s, string expectedPalindrome, string message)
+        {
+            var longestPalindrome = LongestPalindrome(s);
+            Assert.That(longestPalindrome, Is.EqualTo(expectedPalindrome), message);
+        }
+
+        public string LongestPalindrome(string s)
+        {
+            if (s.Length == 1) return s;
+
+            var oddPalindrome = GetLongestOddLengthPalindrome(s);
+            var evenPalindrome = GetLongestEvenLengthPalindrome(s);
+
+            return oddPalindrome.Length > evenPalindrome.Length
+                ? oddPalindrome
+                : evenPalindrome;
+        }
+
+        private static string GetLongestEvenLengthPalindrome(string s)
+        {
+            if (s.Length == 2 && s[0] == s[1]) return s;
+
+            string longestPalindrome = string.Empty;
+
+            int leftIdx = 0;
+            int rightIdx = 0;
+
+            for (var rightMiddleIdx = 1; rightMiddleIdx < s.Length; ++rightMiddleIdx)
+            {
+                var leftMiddleIdx = rightMiddleIdx - 1;
+                if (s[leftMiddleIdx] != s[rightMiddleIdx]) continue;
+
+                for (var offset=1; offset < s.Length; ++offset)
+                {
+                    leftIdx = leftMiddleIdx - offset;
+                    rightIdx = rightMiddleIdx + offset;
+                    if (leftIdx < 0) break;
+                    if (rightIdx >= s.Length) break;
+
+                    if (s[leftIdx] != s[rightIdx]) break;
+                }
+
+                // Previous pair of leftIdx-rightIdx was a palindrome
+                var length = rightIdx - leftIdx - 1;
+                var currentPalindrome = s.Substring(leftIdx + 1, length);
+                if (currentPalindrome.Length > longestPalindrome.Length)
+                {
+                    longestPalindrome = currentPalindrome;
+                }
+            }
+
+            return longestPalindrome;
+        }
+
+        private static string GetLongestOddLengthPalindrome(string s)
+        {
+            string longestPalindrome = string.Empty;
+
+            int leftIdx = 0;
+            int rightIdx = 0;
+
+            for (var middlePos = 0; middlePos < s.Length; ++middlePos)
+            {
+                for (var offset = 1; offset < s.Length; ++offset)
+                {
+                    leftIdx = middlePos - offset;
+                    rightIdx = middlePos + offset;
+                    if (leftIdx < 0) break;
+                    if (rightIdx >= s.Length) break;
+
+                    if (s[leftIdx] != s[rightIdx]) break;
+                }
+
+                // Previous pair of leftIdx-rightIdx was a palindrome
+                var length = (rightIdx - leftIdx - 1);
+                var currentPalindrome = s.Substring(leftIdx + 1, length);
+                if (currentPalindrome.Length > longestPalindrome.Length)
+                {
+                    longestPalindrome = currentPalindrome;
+                }
+            }
+
+            return longestPalindrome;
+        }
+    }
+}
---
+++ Puzzles.LeetCode/Problems_0001_0100/Problem_0006_ZigZagConversion.cs
@@ -0,0 +1,125 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Puzzles.LeetCode.Problems_0001_0100
+{
+    /// <summary>
+    /// The string "PAYPALISHIRING" is written in a zigzag pattern on a given number of rows like this: 
+    /// (you may want to display this pattern in a fixed font for better legibility)
+    ///
+    /// P   A   H   N
+    ///  A P L S I I G
+    ///   Y   I   R
+    /// And then read line by line: "PAHNAPLSIIGYIR"
+    /// Write the code that will take a string and make this conversion given a number of rows:
+    ///
+    /// string convert(string text, int nRows);
+    /// convert("PAYPALISHIRING", 3) should return "PAHNAPLSIIGYIR".
+    /// i.e. this is the ZigZag/Railroad cipher
+    /// </summary>
+    [TestFixture]
+    public class Problem_0006_ZigZagConversion
+    {
+        [Test]
+        public void RunExample()
+        {
+            var result = Convert("PAYPALISHIRING", 3);
+            Assert.That(result, Is.EqualTo("PAHNAPLSIIGYIR"));
+        }
+
+        [Test]
+        [TestCase("TRUSTTHISDOCUMENTALWAYS", 3, "TTSUTARSTIDCMNAWYUHOELS")]
+        [TestCase("NOTALLPDFREADERSARECREATEDEQUAL", 3, "NLFDAREUOALDRAESRCETDQATPEREAEL")]
+        [TestCase("AB", 1, "AB")]
+        public void RunAlternateExamples(string plain, int numRows, string encrypted)
+        {
+            var result = Convert(plain, numRows);
+            Assert.That(result, Is.EqualTo(encrypted));
+        }
+
+        [Test]
+        [TestCase(1, new[] { 1,1,1,1,1})]
+        [TestCase(2, new[] { 1,2,1,2,1,2})]
+        [TestCase(3, new[] { 1,2,3,2,1,2,3,2,1})]
+        [TestCase(4, new[] { 1,2,3,4,3,2,1,2,3,4,3,2,1})]
+        [TestCase(5, new[] { 1,2,3,4,5,4,3,2,1,2,3,4,5,4,3,2,1})]
+        public void ConfirmRailSelection(int numRails, int[] expectedSequence)
+        {
+            var railSelector = new RailSelector(numRails, expectedSequence.Length);
+
+            var idx=0;
+            foreach(var nextRail in railSelector.NextRail())
+            {
+                Assert.That(nextRail, Is.EqualTo(expectedSequence[idx]), string.Format("Rails:{0} Pos:{1}", numRails, idx));
+                idx++;
+            }
+        }
+
+        public string Convert(string s, int numRows)
+        {
+            var rows = new List<string>();
+            for(var rail=0; rail < numRows; ++rail)
+            {
+                rows.Add(string.Empty);
+            }
+
+            var railSelector = new RailSelector(numRows, s.Length);
+
+            var idx = 0;
+            foreach(var nextRail in railSelector.NextRail())
+            {
+                rows[nextRail-1] += s[idx];
+                idx++;
+            }
+
+            var builder = new StringBuilder();
+            foreach(var railText in rows)
+            {
+                builder.Append(railText);
+            }
+            return builder.ToString();
+        }
+
+        public class RailSelector
+        {
+            private readonly int numberOfRails;
+            private readonly int length;
+
+            private int rail = 0;
+            private bool ascending = true;
+
+            public RailSelector(int numberOfRails, int length)
+            {
+                this.numberOfRails = numberOfRails;
+                this.length = length;
+            }
+
+            public IEnumerable<int> NextRail()
+            {
+                if (numberOfRails == 1)
+                {
+                    for (var idx = 0; idx < length; ++idx)
+                    {
+                        yield return 1;
+                    }
+                    yield break;
+                }
+
+                for (var idx = 0; idx < length; ++idx)
+                {
+                    if (rail == 1) ascending = true;
+                    if (rail == numberOfRails) ascending = false;
+
+                    var increment = ascending ? 1 : -1;
+                    rail += increment;
+
+                    yield return rail;
+                }
+            }
+        }
+    }
+}
